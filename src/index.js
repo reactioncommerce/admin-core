035fbdfb1dd4c09c416ed16818f708a0d4c2ecb6
Mike Murray <hellomikemurray@gmail.com>
2020-03-27 15:21:22 -0700

    feat: use App component
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-import { Reaction, registerPlugin } from "../package/src";
+/* eslint-disable react/no-multi-comp */
 import React from "react";
+import { Reaction, registerPlugin, App } from "../package/src";
 import config from "./reaction.config";
 
 // Register fake plugins for testing
-registerPlugin(() => {
-  return {
-    route: "/test-1",
-    navTitle: "Test 1",
-    MainComponent: () => (<span>Route Test 1</span>)
-  }
-})
+registerPlugin(() => ({
+  route: "/test-1",
+  navTitle: "Test 1",
+  // eslint-disable-next-line react/display-name
+  MainComponent: () => (<span>Route Test 1</span>)
+}));
 
-registerPlugin(() => {
-  return {
-    route: "/test-2",
-    navTitle: "Test 2",
-    MainComponent: () => (<span>Route Test 2</span>)
-  }
-})
+registerPlugin(() => ({
+  route: "/test-2",
+  navTitle: "Test 2",
+  // eslint-disable-next-line react/display-name
+  MainComponent: () => (<span>Route Test 2</span>)
+}));
 
 // Configure and "start" the Reaction webapp
 Reaction({
+  AppComponent: App,
   title: "Reaction Admin Core Lib",
   ...config
 });
