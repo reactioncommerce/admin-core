8971d8aa99966f0399f20a75a7096729af64d0bb
Mike Murray <hellomikemurray@gmail.com>
2020-04-06 17:42:31 -0700

    feat: add ContentLayout component
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

---
+++ package/src/ContentLayout/ContentLayout.js
@@ -0,0 +1,81 @@
+import React from "react";
+import PropTypes from "prop-types";
+import clsx from "clsx";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100vw",
+    flexGrow: 1,
+    transition: "padding 225ms cubic-bezier(0, 0, 0.2, 1) 0ms"
+  },
+  standardContent: {
+    maxWidth: 1140,
+    paddingTop: theme.mixins.toolbar.minHeight + (theme.spacing(2)),
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2),
+    paddingBottom: theme.spacing(2),
+    margin: "0 auto"
+  },
+  wideContent: {
+    width: "100vw",
+    paddingTop: theme.mixins.toolbar.minHeight + (theme.spacing(3)),
+    paddingLeft: theme.spacing(3),
+    paddingRight: theme.spacing(3),
+    paddingBottom: theme.spacing(3),
+    flexGrow: 1,
+    transition: "padding 225ms cubic-bezier(0, 0, 0.2, 1) 0ms"
+  },
+  fullContent: {
+    width: "100vw",
+    height: "100vh",
+    paddingTop: theme.mixins.toolbar.minHeight,
+    flexGrow: 1,
+    transition: "padding 225ms cubic-bezier(0, 0, 0.2, 1) 0ms",
+    overflow: "hidden"
+  },
+  leadingDrawerOpen: {
+    paddingLeft: theme.dimensions.drawerWidth
+  },
+  trailingDrawerOpen: {
+    paddingRight: theme.dimensions.detailDrawerWidth
+  }
+}));
+
+const ContentLayout = ({
+  children,
+  isLeadingDrawerOpen,
+  isTrailingDrawerOpen,
+  size
+}) => {
+  const classes = useStyles();
+
+  return (
+    <div
+      className={
+        clsx(classes.root, {
+          [classes[`${size}Content`]]: true,
+          [classes.leadingDrawerOpen]: isLeadingDrawerOpen,
+          [classes.trailingDrawerOpen]: isTrailingDrawerOpen
+        })
+      }
+    >
+      {children}
+    </div>
+  );
+};
+
+ContentLayout.propTypes = {
+  children: PropTypes.node,
+  classes: PropTypes.object,
+  isLeadingDrawerOpen: PropTypes.bool,
+  isMobile: PropTypes.bool,
+  isTrailingDrawerOpen: PropTypes.bool,
+  size: PropTypes.oneOf(["standard", "wide", "full"])
+};
+
+ContentLayout.defaultProps = {
+  size: "standard"
+};
+
+export default ContentLayout;
