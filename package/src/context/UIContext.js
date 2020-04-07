2d2532af5aaa3f164866ebf3d5b09c9cab429d4f
Mike Murray <hellomikemurray@gmail.com>
2020-04-06 17:42:48 -0700

    feat: add UIContext
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

---
+++ package/src/context/UIContext.js
@@ -0,0 +1,13 @@
+import { createContext } from "react";
+
+export const UIContext = createContext({
+  isDetailDrawerOpen: false,
+  isMobile: false,
+  isPrimarySidebarOpen: true,
+  onCloseDetailDrawer: () => { },
+  onClosePrimarySidebar: () => { },
+  onToggleDetailDrawer: () => { },
+  onTogglePrimarySidebar: () => { },
+  setDetailDrawerOpen: () => { },
+  setPrimarySidebarOpen: () => { }
+});
