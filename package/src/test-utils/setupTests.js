75dcfa09ea144973324107d2719e83c2b203e2cd
Mike Murray <hellomikemurray@gmail.com>
2020-04-13 14:39:34 -0700

    fix: move test setup to scripts and import required modules
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- package/src/test-utils/setupTests.js
+++
@@ -1,8 +0,0 @@
-// // Add async/await features
-// import "regenerator-runtime/runtime";
-
-// // add some helpful assertions
-import "@testing-library/jest-dom/extend-expect";
-
-// // Polyfill fetch
-// import "unfetch/polyfill";
