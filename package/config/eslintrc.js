ec72058415826da713cfa42e4bc9602ea1946179
Mike Murray <hellomikemurray@gmail.com>
2020-04-14 14:28:00 -0700

    fix: eslint disable extraneous import
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- package/config/eslintrc.js
+++ package/config/eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
   rules: {
     "id-length": ["error", { exceptions: ["t", "_"] }],
     "jsx-a11y/label-has-for": "off",
+    "node/no-extraneous-import": "off",
     "node/no-missing-import": "off",
     "node/no-missing-require": "off",
     "node/no-unsupported-features/es-syntax": "off",
